721ecbd06ec9503285043bd1afc95231e1149dc6
Vermodje <mr.ryabchikov@list.ru>
2019-08-05 14:26:20 +0300

    05.08.2019 - add Repository

--- src/main/java/base/service/UserDetailsServiceImpl.java
+++ src/main/java/base/service/UserDetailsServiceImpl.java
@@ -1,6 +1,6 @@
 package base.service;
 
-import base.dao.UserDao;
+import base.repository.UserRepository;
 import base.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,12 +10,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
+    /*@Autowired
     private UserDao dao;
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         User user = dao.getUserByLogin(login);
+        if (user == null) {
+            throw new UsernameNotFoundException("User with login: " + login + "not found");
+        }
+        return user;
+    }*/
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.getUserByLogin(login);
         if (user == null) {
             throw new UsernameNotFoundException("User with login: " + login + "not found");
         }
--- src/main/java/base/service/UserServiceImpl.java
+++ src/main/java/base/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package base.service;
 
-import base.dao.UserDao;
+import base.repository.UserRepository;
 import base.exception.UserException;
 import base.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,37 +9,73 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService{
-    @Autowired
-    private UserDao userDao;
+public class UserServiceImpl implements UserService {
 
+    /* Using DAO */
+    //    @Autowired
+//    private UserDao userDao;
+//
+//
+//    @Override
+//    public void add(User user) throws UserException {
+//        userDao.insertUser(user);
+//    }
+//
+//    @Override
+//    public List getAllUsers() {
+//        return userDao.getAllUsers();
+//    }
+//
+//    @Override
+//    public User getUserById(Long id) throws UserException {
+//        return userDao.getUserById(id);
+//    }
+//
+//    @Override
+//    public void deleteUser(Long id) throws UserException {
+//        userDao.deleteUser(id);
+//    }
+//
+//    @Override
+//    public void updateUser(User user) throws UserException {
+//        userDao.updateUser(user);
+//    }
+//
+//    @Override
+//    public User getUserByLogin(String login) {
+//        return userDao.getUserByLogin(login);
+//    }
+
+    /* Using Spring DATA */
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public void add(User user) throws UserException {
-        userDao.insertUser(user);
+        userRepository.save(user);
     }
-
     @Override
     public List getAllUsers() {
-        return userDao.getAllUsers();
+        return (List) userRepository.findAll();
     }
 
     @Override
     public User getUserById(Long id) throws UserException {
-        return userDao.getUserById(id);
+        return userRepository.findById(id).orElseThrow( UserException::new);
     }
 
     @Override
     public void deleteUser(Long id) throws UserException {
-        userDao.deleteUser(id);
+       userRepository.delete(getUserById(id));
     }
 
     @Override
     public void updateUser(User user) throws UserException {
-        userDao.updateUser(user);
+       userRepository.save(user);
     }
 
     @Override
     public User getUserByLogin(String login) {
-        return userDao.getUserByLogin(login);
+        return userRepository.getUserByLogin(login);
     }
+
 }
