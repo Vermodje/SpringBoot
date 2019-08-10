6e3df86a2b14170869f04690764cd7f2c4602b36
Vermodje <mr.ryabchikov@list.ru>
2019-08-10 16:56:56 +0300

    10.08.2019

--- src/main/java/base/controller/UserController.java
+++ src/main/java/base/controller/UserController.java
@@ -1,12 +1,14 @@
 package base.controller;
 
 
+import base.exception.UserException;
 import base.model.Role;
 import base.model.User;
 import base.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,18 +22,15 @@ public class UserController {
     private UserService service;
 
     @RequestMapping("/admin")
-    public ModelAndView admin(Model model, @RequestParam(value = "message", required = false) String message,
-                              @RequestParam(value = "error", required = false) String error) {
+    public ModelAndView admin(Model model) {
         ModelAndView mv = new ModelAndView("admin");
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String currentUserLogin = ((UserDetails) principal).getUsername();
         mv.addObject("users", service.getAllUsers());
         model.addAttribute("user", new User());
-        if (message != null) {
-            mv.addObject("message", "User with same login already exist, Please try again");
-        }
-        if (error != null) {
-            mv.addObject("error", "User with that id is not found");
-        }
+        mv.addObject("currentUser", currentUserLogin);
         return mv;
+
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -58,48 +57,53 @@ public class UserController {
 
     @RequestMapping("/admin/insert")
     public String insertUser(@ModelAttribute("user") User user, Model model) {
+
         try {
             service.add(user);
-        } catch (Exception e) {
-            model.addAttribute("message", new String());
+        } catch (UserException e) {
+            e.printStackTrace();
         }
-        return "redirect:/admin";
-    }
-
-   /* @RequestMapping("/admin/edit")
-    public ModelAndView editUser(@RequestParam long id, @RequestParam(value = "message", required = false) String message, Model model) {
-        ModelAndView mv = new ModelAndView("edit");
-        if (message != null) {
-            mv.addObject("message", "User with same login already exist, Please try again");
-        }
-        try {
-            mv.addObject("user", service.getUserById(id));
-        } catch (Exception e) {
-            model.addAttribute("message", new String());
+        finally {
+            return "redirect:/admin";
         }
 
-        return mv;
-    }*/
+        }
+
+
 
     @RequestMapping("/admin/delete")
     public String deleteUser(@RequestParam long id, Model model) {
+
         try {
             service.deleteUser(id);
-        } catch (Exception e) {
-            model.addAttribute("error", new String());
+        } catch (UserException e) {
+            e.printStackTrace();
         }
-        return "redirect:/admin";
+        finally {
+            return "redirect:/admin";
+        }
+
+
+
+
+
     }
 
     @RequestMapping("/admin/update")
     public String updateUser(@ModelAttribute("user") User user, Model model, @RequestParam long id) {
+
         try {
             service.updateUser(user);
-        } catch (Exception e) {
-            model.addAttribute("id", id);
-            model.addAttribute("message", new String());
-            return "redirect:/login";
+        } catch (UserException e) {
+            e.printStackTrace();
         }
-        return "redirect:/admin";
+        finally {
+            return "redirect:/admin";
+        }
+
+
+
+
+
     }
 }
